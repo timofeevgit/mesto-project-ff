fd7df5e0645417199207fc8ca43dbd264b3fbff3
Dmitry Timofeev <dimadimajob@gmail.com>
2023-11-06 13:50:21 +0300

    final

--- scripts/index.js
+++ scripts/index.js
@@ -30,33 +30,3 @@ function deleteCard(cardElement) {
     placesList.append(cardItem);
   });
 
-
-
-
-
-// function createCard(data) {
-//   const cardElement = template.content.querySelector(".card").cloneNode(true);
-//   const cardImage = cardElement.querySelector(".card__image");
-//   cardImage.src = data.link;
-//   cardImage.alt = data.name;
-//   const cardTitle = cardElement.querySelector(".card__title");
-//   cardTitle.textContent = data.name;
-//   const deleteButton = cardElement.querySelector(".card__delete-button");
-//   deleteButton.addEventListener("click", () => {
-//     deleteCard(cardElement);
-//   });
-//   return cardElement;
-// }
-
-
-
-// function deleteCard(cardElement) {
-//   cardElement.remove();
-// }
-
-
-
-// initialCards.forEach((card) => {
-//   const cardElement = createCard(card);
-//   placesList.appendChild(cardElement);
-// });
